1f1058e05a74ceec42974c3a621641ce8308edf3
Russell Mirabelli <russell@mirabellifamily.org>
2019-11-13 12:50:36 -0600

    Initial commit

---
+++ Homework7.playground/Contents.swift
@@ -0,0 +1,52 @@
+import Foundation
+
+// For this homework project, we'll be connecting to the "TLDR" server
+// to add a few books. The first thing you need to do is create an object
+// that we'll upload to the server.
+
+// MARK: - STEP ONE
+
+// Implement a struct that defines a book. A book consists of the following
+// items:
+// A title, which is a string
+// An author, which is a string
+// The publication year, as a string (because dates are hard)
+// A string for the URL for an image for the book cover
+//
+// Remember that this structure needs to conform to the `Encodable` protocol.
+// Using `Codable` more generally will be useful, as by doing this you'll
+// be able to reuse this struct in Project Three.
+
+// MARK: - STEP TWO
+
+// Once you've defined this structure, you'll need to define a couple of
+// book objects that you can insert into the database. In order or us to
+// have an amusing dataset to work with, each student is requested to
+// create five different books for this database.
+
+// MARK: - STEP THREE
+
+// Now we need to publish this data to the server.
+
+// Create a URL to connect to the server. Its address is:
+//      https://uofd-tldrserver-develop.vapor.cloud/books
+
+// Create a URL request to publish the information, based upon the URL you
+// just created.
+
+// Add the body to the URL request you just created, by using JSONEncoder.
+
+// Add a "Content-Type" : "application/json" header to your request, so that
+// the server will properly understand the body of your request.
+
+// Set the method of the request to "POST", because we're providing information
+// rather than retrieving it.
+
+// Create a data task for publishing this element, and kick it off with a resume().
+
+// MARK: - HELPFUL HINTS
+// You might want to create a method for publishing the data, so that you
+// can effectively loop over an array of books.
+//
+// If you visit the URL for the service in a 'GET' request, it will return a
+// list of books to you. We'll be using this list of books for Project Three.
---
+++ README.md
@@ -0,0 +1,4 @@
+# uofd-homework7
+Homework 7 for mobile app development
+
+All instructions are in the playground.
